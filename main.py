fccbdc91e8de62c612055fe1e30c0620b2a2b311
counterat <counteratios@gmail.com>
2024-06-23 13:35:49 +0300

    SDSD

--- main.py
+++ main.py
@@ -69,10 +69,10 @@ async def update_all_users_energy():
 
 scheduler = AsyncIOScheduler()
 
-#scheduler.add_job(update_all_users_energy, 'interval', seconds=2)
+scheduler.add_job(update_all_users_energy, 'interval', seconds=2)
 #scheduler.add_job(click_for_autoclicker_users, 'interval', seconds=60)
 #scheduler.add_job(update_users_leaderboard, 'interval', seconds=1)
-scheduler.add_job(update_squads_leaderboard, 'interval', seconds=1) 
+#scheduler.add_job(update_squads_leaderboard, 'interval', seconds=1) 
 #scheduler.add_job(update_all_users_income_per_day, CronTrigger(hour=0, minute=0))
 
 scheduler.start()
