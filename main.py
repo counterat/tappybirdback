778e7ae00336bfcc5d874b69002d3599730279c4
counterat <counteratios@gmail.com>
2024-06-26 22:40:10 +0300

    asdassdasd

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ async def update_all_users_energy():
 
 
 scheduler = AsyncIOScheduler()
-scheduler.add_job(click_for_autoclicker_users, 'interval', seconds=2)
+scheduler.add_job(click_for_autoclicker_users, 'interval', seconds=60)
 scheduler.add_job(update_all_users_energy, 'interval', seconds=2) 
 
 scheduler.add_job(update_users_leaderboard, 'interval',  minutes=1) 
